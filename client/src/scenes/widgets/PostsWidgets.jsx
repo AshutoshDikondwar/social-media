7f7bd320588e8119fb2906e31681b6c1efb90ef0
Ashutosh <iamashutoshdikondwar@gmail.com>
2023-10-10 03:49:04 +0530

    vercel frontend

--- client/src/scenes/widgets/PostsWidgets.jsx
+++ client/src/scenes/widgets/PostsWidgets.jsx
@@ -9,7 +9,7 @@ const PostsWidgets = ({ userId, isProfile = false }) => {
     const token = useSelector((state) => state.token)
 
     const getPosts = async () => {
-        const response = await fetch(`http://localhost:3001/posts`, {
+        const response = await fetch(`https://connect-beta-three.vercel.app/posts`, {
             method: "GET",
             headers: { Authorization: `Bearer ${token}` },
         })
@@ -17,7 +17,7 @@ const PostsWidgets = ({ userId, isProfile = false }) => {
         dispatch(setPosts({ posts: data }))
     }
     const getUserPosts = async () => {
-        const response = await fetch(`http://localhost:3001/posts/${userId}/posts`, {
+        const response = await fetch(`https://connect-beta-three.vercel.app/posts/${userId}/posts`, {
             method: "GET",
             headers: { Authorization: `Bearer ${token}` },
         })
